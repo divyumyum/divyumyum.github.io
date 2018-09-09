29c691489a36b604ffe9a4115ba3392c0ce786ee
divyumyum <adivyam5@gmail.com>
2018-09-09 23:09:51 +0530

    Count
    Count

--- js/main.js
+++ js/main.js
@@ -89,7 +89,7 @@
     * ------------------------------------------------------ */
     var ssFinalCountdown = function() {
 
-        var finalDate = '2018/11/15';
+        var finalDate = '2018/11/01';
 
         $('.home-content__clock').countdown(finalDate)
         .on('update.countdown finish.countdown', function(event) {
